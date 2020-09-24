b928bc1bd77987557b16e629f3c49d9bd9fc8122
KarthikManoj <manojdeen12@gmail.com>
2020-09-24 15:44:35 +0530

    First commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import * as ReactBootstrap from "react-bootstrap";
+import UploadBookPage from './components/views/uploadBook/UploadBookPage.js'
+import Switch from 'react-bootstrap/esm/Switch';
+import { Route } from 'react-router';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ReactBootstrap.Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+      <ReactBootstrap.Navbar.Brand href="#home">International School Books</ReactBootstrap.Navbar.Brand>
+      <ReactBootstrap.Navbar.Toggle aria-controls="responsive-navbar-nav" />
+      <ReactBootstrap.Navbar.Collapse id="responsive-navbar-nav">
+      <ReactBootstrap.Nav className="mr-auto">
+      <ReactBootstrap.Nav.Link href="#features">Books</ReactBootstrap.Nav.Link>
+      
+      </ReactBootstrap.Nav>
+      <ReactBootstrap.Nav>
+      <ReactBootstrap.Nav.Link href="#deets">Login</ReactBootstrap.Nav.Link>
+      <ReactBootstrap.Nav.Link eventKey={2} href="#memes">
+        Register
+      </ReactBootstrap.Nav.Link>
+    </ReactBootstrap.Nav>
+
+
+    {/* <ReactBootstrap.Form inline>
+      <ReactBootstrap.FormControl type="text" placeholder="Search" className="mr-sm-2" />
+      <ReactBootstrap.Button variant="outline-light">Search</ReactBootstrap.Button>
+    </ReactBootstrap.Form> */}
+
+  </ReactBootstrap.Navbar.Collapse>
+</ReactBootstrap.Navbar>
     </div>
   );
 }
