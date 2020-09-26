76887fabaddfd3efdd87f5c6159a20868c9939be
WijeAkshi <akshiwije@gmail.com>
2020-09-26 17:11:49 +0530

    Akshi

--- src/App.js
+++ src/App.js
@@ -2,36 +2,21 @@ import React from 'react';
 import './App.css';
 import * as ReactBootstrap from "react-bootstrap";
 import UploadBookPage from './components/views/uploadBook/UploadBookPage.js'
-import Switch from 'react-bootstrap/esm/Switch';
-import { Route } from 'react-router';
 
+
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+
+import Navigation from "./components/Navigation";
+import AddToCart from "./components/views/addCart/addToCart";
 function App() {
   return (
-    <div className="App">
-      <ReactBootstrap.Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
-      <ReactBootstrap.Navbar.Brand href="#home">International School Books</ReactBootstrap.Navbar.Brand>
-      <ReactBootstrap.Navbar.Toggle aria-controls="responsive-navbar-nav" />
-      <ReactBootstrap.Navbar.Collapse id="responsive-navbar-nav">
-      <ReactBootstrap.Nav className="mr-auto">
-      <ReactBootstrap.Nav.Link href="#features">Books</ReactBootstrap.Nav.Link>
-      
-      </ReactBootstrap.Nav>
-      <ReactBootstrap.Nav>
-      <ReactBootstrap.Nav.Link href="#deets">Login</ReactBootstrap.Nav.Link>
-      <ReactBootstrap.Nav.Link eventKey={2} href="#memes">
-        Register
-      </ReactBootstrap.Nav.Link>
-    </ReactBootstrap.Nav>
+      <BrowserRouter>
+          <Navigation/>
 
-
-    {/* <ReactBootstrap.Form inline>
-      <ReactBootstrap.FormControl type="text" placeholder="Search" className="mr-sm-2" />
-      <ReactBootstrap.Button variant="outline-light">Search</ReactBootstrap.Button>
-    </ReactBootstrap.Form> */}
-
-  </ReactBootstrap.Navbar.Collapse>
-</ReactBootstrap.Navbar>
-    </div>
+          <Switch>
+        <Route path="/addCart" component={AddToCart}></Route>
+          </Switch>
+      </BrowserRouter>
   );
 }
 
