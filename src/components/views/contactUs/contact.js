3db867d8fe8091bf9f0dcec5b0694d65fa4c649a
NiroNisha698 <nironisha0606@gmail.com>
2020-09-29 21:15:47 +0530

    nirosha tested contactus page

---
+++ src/components/views/contactUs/contact.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { useForm } from "react-hook-form";
+
+function App() {
+    const { register, handleSubmit } = useForm();
+    const onSubmit = data => console.log(data);
+    return (
+        <div className="App">
+            <form onSubmit={handleSubmit(onSubmit)}>
+                <input name="firstName" ref={register} />
+                <select name="gender" ref={register}>
+                    <option value="female">female</option>
+                    <option value="male">male</option>
+                    <option value="other">other</option>
+                </select>
+                <input type="submit" />
+            </form>
+ </div>
+    );
+}
+
+export default App;
